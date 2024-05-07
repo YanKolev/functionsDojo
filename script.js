99f72697bef5b977933f128979d3c9c3dd9bb9e2
Yan Kolev <y.kolev11@gmail.com>
2024-05-07 19:17:02 +0300

    Call/ Apply / Bind Methods

--- script.js
+++ script.js
@@ -115,3 +115,139 @@ document.body.addEventListener('click', high5)
 //The biggest advantage of call functions is that it makes easy to split our code into more re-usable and readable parts
 //2nd advantage- they allow us to make abstraction
 
+
+
+// ----- Functions returning functions ----
+
+const greet = function(greeting){
+    return function(name){
+        console.log(`${greeting} ${name}`)
+    }
+}
+
+const greeterHey = greet('Hey')
+greeterHey('John');
+greeterHey('Steven');
+
+greet('Hello')('John');
+
+//arrowfunction conversion of the greet function
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Hi')('John');
+
+
+
+//-- Call and Apply Methods --
+
+const lufthansa = {
+    airline: 'lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    book(flightNum, name) {
+        console.log(`
+        ${name} booked a seat on ${this.airline} 
+        flight ${this.iataCode} ${flightNum}`
+    );
+    this.bookings.push({flight: `${this.iataCode} ${flightNum}`, name})
+    },
+};
+
+lufthansa.book(239, 'John Smith');
+lufthansa.book(635, 'Mike Rogers');
+
+const eurowings = {
+    name: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+};
+
+//create a new function 
+const book = lufthansa.book
+
+// book(23, 'Randy Orton'); // it will point undefined / book function is regular function call, so the this keyword will point into undefined.
+
+book.call(eurowings, 23, 'Randy Orton'); // we called the call method that calls the book function with the this keyword set to eurowings.
+//manually making the this keyword to any function that we can to call 
+console.log(eurowings);
+
+book.call (lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+//the first argument manually sets to where this call method should look
+//manually manipulating the this keyword
+
+const swiss = {
+    airline: 'Swis Air Lines', // the format should be the same 
+    iataCode: 'LX',
+    bookings: []
+}
+
+book.call (swiss, 583, 'John Dozzer');
+console.log(swiss)
+
+
+//apply method does the same thing as the call method earlier, 
+//apply does not receive a list of arguments after the this word but it will take an array 
+
+const flightData = [583, 'DB Cooper']
+book.apply(swiss,flightData);
+console.log(swiss);
+//apply is not that used anymore in JS.
+
+
+book.call(swiss, ...flightData); //the spread operator is overtaking the apply method.
+
+//--- Bind method --- 
+
+//allow us to manually set the this keyword for any function call.
+//the difference is that bind does not immediately call the function, 
+//instead it returns a new function where the this keyword is bound its set to whatever value we set to bind. 
+
+//book.call(eurowings, 23, 'Sarah Connor');
+
+const bookEW  = book.bind(eurowings); //this will not call the function it will create a new function where this is set to eurowings
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23,'Steve Williams');
+
+const bookEW23 = book.bind(eurowings, 23); //adding specific parameters to the bind function
+bookEW23('John Smith');
+bookEW23('Mimmy Cooper');
+//basically pre-set 23
+
+
+//With Event Listeners
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function(){
+    console.log(this);
+    
+    this.planes++;
+    console.log(this.planes); 
+
+};
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+//Partial application of the bind method
+
+//we are not even used about the this keyword, but we use bind- >
+//with partial application we are pre-setting parameters
+
+const addTax = (rate, value ) => value + value*rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null,0.23); //manually setting the rate at 23%, we need to use null as the first argumets is this keyword. but we do not need it
+
+console.log(addVAT(100));
+
+/// challenge
+const addTaxRate = function(rate){
+    return function(value){
+        return value + value*rate;
+    }
+}
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
