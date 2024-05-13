a934710a89e8bfe2d297b79d9921e665be82313c
Yan Kolev <y.kolev11@gmail.com>
2024-05-13 19:28:49 +0300

    adding pseudo code

--- challenges.js
+++ challenges.js
@@ -46,3 +46,18 @@ const poll = {
     // This generates [0, 0, 0, 0]. More in the next section!
     answers: new Array(4).fill(0),
     };
+
+    /*
+    Main components of the poll app: 
+    Ask a question,-> prompt
+    receive answer-> store the result
+    update answers array properly depending on position
+    check if the input is a number and inpit makes sense with the options provided
+    call the method when the user clicks answer
+    New method to display the poll results : "The
+    method takes a string as an input (called 'type'), which can be either 'string'
+    or 'array'. If type is 'array', simply display the results array as it is, using
+    console.log(). This should be the default option. If type is 'string', display a
+    string like "Poll results are 13, 2, 4, 1"."
+    Run the 'displayResults' method at the end of each
+    'registerNewAnswer' method call. */
