50efcd0bf6bc0cf6583059a6ae376c47493f59b8
Yan Kolev <y.kolev11@gmail.com>
2024-05-19 18:03:43 +0300

    Adding solution

--- challenges.js
+++ challenges.js
@@ -45,8 +45,40 @@ const poll = {
     options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
     // This generates [0, 0, 0, 0]. More in the next section!
     answers: new Array(4).fill(0),
+
+   registerNewAnswer(){
+    //Get answer
+        const answer = Number(
+            prompt(
+                `${this.question}\n${this.options.join('\n')}
+                \n(Write option number)`
+            )
+        );
+        console.log(answer);
+
+        //reguster answer
+        typeof answer === 'number'&& answer <this.answers.length && this.answers[answer]++;
+
+        //console.log(this.answers)
+        this.displayResults();
+        this.displayResults('string');
+
+   },
+        displayResults(type = 'array'){
+            if (type === 'array'){
+                console.log(this.answers);
+            } else if (type === 'string'){
+                console.log(`Poll results are ${this.answers.join(', ')}`);
+            }
+        }
+    
     };
 
+    //poll.registerNewAnswer();
+
+    document.querySelector('.poll').addEventListener('click',poll.registerNewAnswer.bind(poll))
+    
+    poll.displayResults.call({answers: [5,2, 3]},'string');
     /*
     Main components of the poll app: 
     Ask a question,-> prompt
@@ -61,3 +93,4 @@ const poll = {
     string like "Poll results are 13, 2, 4, 1"."
     Run the 'displayResults' method at the end of each
     'registerNewAnswer' method call. */
+
